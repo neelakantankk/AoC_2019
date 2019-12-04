af6140c17eb5d66496327e373df071041ac081f2
Neelakantan K.K <neelakantankk@gmail.com>
2019-12-04 15:29:51 +0530

    Solverd part 2 as well.

---
+++ Problem_04/part_2.py
@@ -0,0 +1,29 @@
+import re
+
+START = 372037
+END = 905157
+
+def is_valid(candidate):
+    cand_str = str(candidate)
+    if not re.search(r"(\d)\1",cand_str):
+        return False
+    for index, digit in enumerate(cand_str[1:]):
+        if int(digit)<int(cand_str[index]):
+            return False
+    counts = [cand_str.count(x) for x in re.findall(r"(\d)\1",cand_str)]
+    if 2 not in counts:
+        return False
+
+    return True
+
+def main():
+    valid_passwords = 0
+    for candidate in range(START, END+1):
+        if is_valid(candidate):
+            valid_passwords+=1
+    print(valid_passwords)
+
+if __name__ == '__main__':
+    main()
+            
+        
